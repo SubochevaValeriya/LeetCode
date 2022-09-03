78b1d95ae7aff995cbf48ecd09da6daece254de6
Valeriya Subocheva <subocheva.valeria@gmail.com>
2022-09-03 13:11:25 +0300

    637. Average of Levels of Binary Tree

---
+++ 637.AverageOfLevelsOfBinaryTree/main.go
@@ -0,0 +1,43 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 3
+// 2 4
+// 2 4 2 4
+
+func averageOfLevels(root *TreeNode) []float64 {
+	answer := []float64{}
+	if root == nil {
+		return answer
+	}
+	sum, count := []int{}, []int{}
+
+	sum, count = byLevels(root, 0, sum, count)
+
+	for i := 0; i < len(sum); i++ {
+		answer = append(answer, float64(sum[i])/float64(count[i]))
+	}
+	return answer
+}
+
+func byLevels(root *TreeNode, i int, sum, count []int) ([]int, []int) {
+	if root == nil {
+		return sum, count
+	}
+	if len(sum)-1 >= i {
+		sum[i] += root.Val
+		count[i] += 1
+	} else {
+		sum = append(sum, root.Val)
+		count = append(count, 1)
+	}
+
+	sum, count = byLevels(root.Left, i+1, sum, count)
+	return byLevels(root.Right, i+1, sum, count)
+
+}
