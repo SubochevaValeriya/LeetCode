46f22eca55139ec5d965fd17f1f5b46986f50055
Valeriya Subocheva <96348645+SubochevaValeriya@users.noreply.github.com>
2022-09-13 22:27:35 +0300

    860. Lemonade Change

--- 860.LemonadeChange/lemonade_change.go
+++ 860.LemonadeChange/lemonade_change.go
@@ -1,5 +1,32 @@
 package main
 
-func lemonadeChange(bills []int) bool {
+// https://leetcode.com/problems/lemonade-change/
 
+func lemonadeChange(bills []int) bool {
+	m := map[int]int{}
+
+	for _, bill := range bills {
+		switch bill {
+		case 10:
+			if m[5] == 0 {
+				return false
+			}
+			m[5]--
+		case 20:
+			if m[5] == 0 {
+				return false
+			}
+			if m[10] > 0 {
+				m[5]--
+				m[10]--
+			} else if m[5] < 3 {
+				return false
+			} else {
+				m[5] -= 3
+			}
+		}
+
+		m[bill]++
+	}
+	return true
 }
--- 860.LemonadeChange/lemonade_change_test.go
+++ 860.LemonadeChange/lemonade_change_test.go
@@ -7,24 +7,21 @@ import (
 
 type TestCase struct {
 	Name     string
-	s        string
-	t        string
+	bills    []int
 	Expected bool
 }
 
 func TestLeetCode(t *testing.T) {
 	testCases := []TestCase{
-		{Name: "case with egg & add", s: "egg", t: "add", Expected: true},
-		{Name: "case with foo & bar ", s: "foo", t: "bar", Expected: false},
-		{Name: "case with paper & title", s: "paper", t: "title", Expected: true},
-		{Name: "case with badc & baba", s: "badc", t: "baba", Expected: false},
+		{Name: "case with [5,5,5,10,20]", bills: []int{5, 5, 5, 10, 20}, Expected: true},
+		{Name: "case with [5,5,10,10,20]", bills: []int{5, 5, 10, 10, 20}, Expected: false},
 	}
 
 	for _, cse := range testCases {
 		cse := cse
 		t.Run(cse.Name, func(t *testing.T) {
-			result := isIsomorphic(cse.s, cse.t)
-			assert.Equalf(t, cse.Expected, result, "for %d and %d expected %t, got %t", cse.s, cse.t, cse.Expected, result)
+			result := lemonadeChange(cse.bills)
+			assert.Equalf(t, cse.Expected, result, "for %d expected %t, got %t", cse.bills, cse.Expected, result)
 		})
 	}
 }
