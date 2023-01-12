ddc57ab332b249fbec4bb64d316160db14fa388c
Valeriya Subocheva <subocheva.valeria@gmail.com>
2023-01-12 13:36:25 +0300

    Solutions

--- 767.ReorganizeString/reorganize_string.go
+++ 767.ReorganizeString/reorganize_string.go
@@ -2,113 +2,93 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-//func main() {
-//	fmt.Println(reorganizeString("aabbcc"))
-//	fmt.Println(reorganizeString("vvvlo"))
-//	fmt.Println(reorganizeString("eqmeyggvp"))
-//}
+func main() {
+	fmt.Println(reorganizeString("aab"))
+	fmt.Println(reorganizeString("aaab"))
+	fmt.Println(reorganizeString("vvvlo"))
+	fmt.Println(reorganizeString("aabbcc"))
+}
 
-//func reorganizeString(s string) string {
-//	m := map[string]int{}
-//	answer := make([]rune, len(s))
-//	alphabet := []rune{}
-//
-//	for i := 0; i < len(s); i++ {
-//		m[string(s[i])]++
-//
-//	}
-//
-//	for letter := 'a'; letter <= 'z'; letter++ {
-//		alphabet = append(alphabet, letter)
-//	}
-//
-//	sort.SliceStable(alphabet, func(i, j int) bool {
-//		return m[string(alphabet[i])] > m[string(alphabet[j])]
-//	})
-//
-//	var theSameCountForLetter = 1
-//	var firstJ = 0
-//
-//	for i := 0; i < len(alphabet); i++ {
-//		count := m[string(alphabet[i])]
-//		if count != 0 {
-//			if i < len(alphabet)-1 {
-//				if count == m[string(alphabet[i+1])] && theSameCountForLetter >= 1 {
-//					theSameCountForLetter++
-//					if m[string(alphabet[i+2])] < count {
-//						answerPart := inAlphabetOrder(alphabet[i-theSameCountForLetter+2:i+2], count)
-//						answer = append(answerPart, answer[len(answerPart):]...)
-//						firstJ += len(answerPart) - 1
-//						if m[string(alphabet[i+2])] == 0 {
-//							return string(answer)
-//						}
-//						i++
-//						theSameCountForLetter = -1
-//					}
-//					continue
-//				}
-//			}
-//			theSameCountForLetter = -1
-//			for j := firstJ; count > 0; {
-//				if j >= len(s) {
-//					return ""
-//				}
-//				if answer[j] != 0 {
-//					j++
-//					continue
-//				} else {
-//					answer[j] = alphabet[i]
-//					j += 2
-//					count--
-//				}
-//			}
-//			m[string(alphabet[i])] = 0
-//		} else {
-//			if i == len(alphabet)-1 {
-//				i = -1
-//			}
-//			continue
-//		}
-//
-//		if len(answer) > 1 {
-//			if answer[len(answer)-1] == answer[len(answer)-2] && answer[len(answer)-1] != 0 {
-//				return ""
-//			}
-//		}
-//
-//		if answer[len(answer)-1] != 0 && answer[len(answer)-2] != 0 {
-//			return string(answer)
-//		}
-//
-//		if i == len(alphabet)-1 {
-//			i = -1
-//		}
-//	}
-//
-//	return string(answer)
-//}
-//
-//func inAlphabetOrder(alphabet []rune, n int) []rune {
-//	answer := []rune{}
-//	for i := 0; i < len(alphabet); i++ {
-//		if n == 0 {
-//			break
-//		}
-//		answer = append(answer, alphabet[i])
-//		if i == len(alphabet)-1 {
-//			n--
-//			i = -1
-//		}
-//	}
-//	return answer
-//}
+func reorganizeString(s string) string {
+	m := map[string]int{}
+	answer := make([]rune, len(s))
+	alphabet := []rune{}
 
-func reorganizeStringInAlphabetOrder(s string) string {
-	if len(s) < 2 {
-		return s
+	for i := 0; i < len(s); i++ {
+		m[string(s[i])]++
+
 	}
+
+	for letter := 'a'; letter <= 'z'; letter++ {
+		alphabet = append(alphabet, letter)
+	}
+
+	sort.SliceStable(alphabet, func(i, j int) bool {
+		return m[string(alphabet[i])] > m[string(alphabet[j])]
+	})
+
+	for i := 0; i < len(alphabet); i++ {
+		count := m[string(alphabet[i])]
+		if count != 0 {
+			for j := 0; count > 0; {
+				if j >= len(s) {
+					return ""
+				}
+				if answer[j] != 0 {
+					j++
+					continue
+				} else {
+					answer[j] = alphabet[i]
+					j += 2
+					count--
+				}
+			}
+			m[string(alphabet[i])] = 0
+		} else {
+			if i == len(alphabet)-1 {
+				i = -1
+			}
+			continue
+		}
+
+		if len(answer) > 1 {
+			if answer[len(answer)-1] == answer[len(answer)-2] && answer[len(answer)-1] != 0 {
+				return ""
+			}
+		}
+
+		if answer[len(answer)-1] != 0 && answer[len(answer)-2] != 0 {
+			return string(answer)
+		}
+
+		if i == len(alphabet)-1 {
+			i = -1
+		}
+	}
+
+	return string(answer)
+}
+
+//needToReorganize := false
+//sRune := []rune(s)
+//for j := 1; j < len(sRune); j++ {
+//	if sRune[i] == sRune[j] {
+//		needToReorganize = true
+//		continue
+//	}
+//	if sRune[i] != sRune[j] && needToReorganize == true {
+//		sRune[i], sRune[j] = sRune[j], sRune[i]
+//	} else {
+//		break
+//	}
+//}
+
+//	return string(sRune)
+
+func reorganizeString0(s string) string {
 	m := map[string]int{}
 	var answer string
 	alphabet := []string{}
@@ -125,16 +105,6 @@ func reorganizeStringInAlphabetOrder(s string) string {
 	for i := 0; i < len(alphabet); i++ {
 
 		count := m[alphabet[i]]
-		if len(s)%2 == 0 {
-			if count >= len(s)/2+1 {
-				return ""
-			}
-		} else {
-			if count > len(s)/2+1 {
-				return ""
-			}
-		}
-
 		if count != 0 {
 			answer += alphabet[i]
 			m[alphabet[i]]--
@@ -145,27 +115,12 @@ func reorganizeStringInAlphabetOrder(s string) string {
 			continue
 		}
 
-		if len(answer) == len(s) {
+		if len(answer) > 1 {
 			if answer[len(answer)-1] == answer[len(answer)-2] {
-				answerRune := []rune(answer)
-				left, right := 0, len(answerRune)-1
-				for right > left {
-					fmt.Println(string(answerRune))
-					if answerRune[right] == answerRune[right-1] {
-						if answerRune[left] != answerRune[right] {
-							if left == 0 {
-								answerRune = append([]rune{answerRune[right]}, answerRune[:right]...)
-							} else if answerRune[left+1] != answerRune[right] {
-								answerSecondPart := append([]rune{answerRune[right]}, answerRune[left+1:right]...)
-								answerRune = append(answerRune[:left+1], answerSecondPart...)
-							}
-						}
-						left++
-					} else {
-						return string(answerRune)
-					}
-				}
+				return ""
 			}
+		}
+		if len(answer) == len(s) {
 			return answer
 		}
 
