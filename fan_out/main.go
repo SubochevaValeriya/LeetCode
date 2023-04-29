95c2b554695b72c7d0e9d07fbcf5ca4239c9c47c
Valeriya Subocheva <subocheva.valeria@gmail.com>
2023-04-29 17:07:10 +0300

    Solutions

--- fan_out/main.go
+++ fan_out/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -17,9 +18,28 @@ func main() {
 		}
 	}()
 
+	eventsQueue := make([]chan SomeEvent, len(listeners))
+
+	for i := 0; i < len(listeners); i++ {
+		eventsQueue[i] = make(chan SomeEvent, 2)
+	}
+
+	for i, listener := range listeners {
+		go func(i int, listener Listener) {
+			for {
+				event := <-eventsQueue[i]
+				listener.Handle(event)
+			}
+		}(i, listener)
+	}
+
 	for event := range events {
-		for _, listener := range listeners {
-			listener.Handle(event)
+		for i, queue := range eventsQueue {
+			if len(queue) == 2 {
+				log.Println("Message is lost from", i)
+				continue
+			}
+			queue <- event
 		}
 	}
 }
@@ -36,7 +56,7 @@ type listener1 struct {
 
 func (l listener1) Handle(e SomeEvent) {
 	time.Sleep(2 * time.Second)
-	fmt.Println("Hello")
+	fmt.Println(time.Now(), "Hello")
 }
 
 type listener2 struct {
