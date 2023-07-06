0c9a8e33c9da6e20cbb96e30cf368fa0a91fff47
Valeriya Subocheva <subocheva.valeria@gmail.com>
2023-07-06 19:08:13 +0300

    Solutions

--- 137.SingleNumberII/main.go
+++ 137.SingleNumberII/main.go
@@ -1,9 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+func main() {
+	fmt.Println(singleNumber([]int{0, 1, 0, 1, 0, 1, 99}))
 }
 
 func singleNumber(nums []int) int {
-
+	sort.Ints(nums)
+	count := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			count++
+			continue
+		}
+		if count != 3 {
+			return nums[i-1]
+		}
+		count = 1
+	}
+	return nums[len(nums)-1]
 }
